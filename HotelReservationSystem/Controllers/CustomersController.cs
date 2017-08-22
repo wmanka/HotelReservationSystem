b07144c96bb61853e5c586a12572b70f90c20065
wmanka <wioletta.manka728@gmail.com>
2017-08-23 00:38:18 +0200

    Add a form to add/update customer

---
+++ HotelReservationSystem/Controllers/CustomersController.cs
@@ -0,0 +1,64 @@
+﻿using HotelReservationSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelReservationSystem.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult New()
+        {
+            var customer = new Customer();
+
+            return View("Form", customer);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View("Form", customer);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+
+        }
+    }
+}
--- HotelReservationSystem/Controllers/HomeController.cs
+++ HotelReservationSystem/Controllers/HomeController.cs
@@ -15,15 +15,6 @@ namespace HotelReservationSystem.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
