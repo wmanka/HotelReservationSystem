cbb909f066efa52537869687f5ecd6b2ee5ce319
wmanka <wioletta.manka728@gmail.com>
2017-08-24 10:27:50 +0200

    Users have to log in to access application

--- HotelReservationSystem/Controllers/HomeController.cs
+++ HotelReservationSystem/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace HotelReservationSystem.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : Controller
     {
         public ActionResult Index()
