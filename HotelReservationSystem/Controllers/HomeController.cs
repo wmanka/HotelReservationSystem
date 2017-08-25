75557508924492755ab34331ef5edada9cc4a181
wmanka <wioletta.manka728@gmail.com>
2017-08-25 14:30:00 +0200

    Disabled Session

--- HotelReservationSystem/Controllers/HomeController.cs
+++ HotelReservationSystem/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ namespace HotelReservationSystem.Controllers
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        //[OutputCache(Duration = 50, Location = System.Web.UI.OutputCacheLocation.Server, VaryByParam = "*")]
         public ActionResult Index()
         {
             return View();
