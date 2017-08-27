8fb13db2ad63ce7768c70162a5d5e7751e6bc5d0
wmanka <wioletta.manka728@gmail.com>
2017-08-27 13:28:40 +0200

    Fixed typeahed bug (not showing all results)

--- HotelReservationSystem/Scripts/typeahead.bundle.js
+++ HotelReservationSystem/Scripts/typeahead.bundle.js
@@ -1720,8 +1720,9 @@
                     suggestions = suggestions || [];
                     if (!canceled && rendered < that.limit) {
                         that.cancel = $.noop;
-                        rendered += suggestions.length;
-                        that._append(query, suggestions.slice(0, that.limit - rendered));
+                        suggestions = (suggestions || []).slice(0, that.limit);
+                        rendered = suggestions.length;
+                        that._append(query, suggestions);
                         that.async && that.trigger("asyncReceived", query);
                     }
                 }
