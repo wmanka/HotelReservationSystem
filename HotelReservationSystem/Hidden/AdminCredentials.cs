b5c64381814a41ae48d2ebd362a553eeb6f7b8ce
Wioletta Mańka <wioletta.manka728@gmail.com>
2019-05-14 17:41:13 +0200

    Update Credentials Information

---
+++ HotelReservationSystem/Hidden/AdminCredentials.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelReservationSystem.Hidden
+{
+    public static class AdminCredentials
+    {
+        public static string Login = "admin@admin.com";
+        public static string Password = "Admin1!";
+    }
+}
---
+++ HotelReservationSystem/Hidden/FacebookData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelReservationSystem.Hidden
+{
+	public static class FacebookData
+	{
+        public static string AppId = "125096521477596";
+        public static string AppSecret = "1c21d471c658034de2441f5257074481";
+    }
+}
